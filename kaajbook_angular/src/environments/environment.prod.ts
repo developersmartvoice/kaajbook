86dc8894a56e4a1042f161161f720bdbfbbc085e
Arafat Hossain <arafat6462@gmail.com>
2023-05-17 15:40:09 +0600

    change login background color, default logo & add api for local, live, subdomain

--- kaajbook_angular/src/environments/environment.prod.ts
+++ kaajbook_angular/src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://127.0.0.1:8000'
-    // apiUrl: 'http://kaajbook.com/kaajbook_laravel/public'
+    
+    // use this apiUrl for local environment
+    // apiUrl: 'http://127.0.0.1:8000'
+
+    // use this apiUrl for live server "kaajbook.com"
+    apiUrl: 'http://kaajbook.com/kaajbook_laravel/public'
+
+    // use this apiUrl for live sub domain "laravelapi.kaajbook.com"
+    // apiUrl: 'http://laravelapi.kaajbook.com/abc_laravel/public'
 };
--- kaajbook_angular/src/environments/environment.ts
+++ kaajbook_angular/src/environments/environment.ts
@@ -1,6 +1,13 @@
 export let environment: { apiUrl: string; production: boolean };
 environment = {
 	production: false,
-	// apiUrl: 'http://kaajbook.com/kaajbook_laravel/public'
-	apiUrl: 'http://127.0.0.1:8000'
+	// use this apiUrl for local environment
+	// apiUrl: 'http://127.0.0.1:8000'
+
+	// use this apiUrl for live server "kaajbook.com"
+	apiUrl: 'http://kaajbook.com/kaajbook_laravel/public'
+
+	// use this apiUrl for live sub domain "laravelapi.kaajbook.com"
+	// apiUrl: 'http://laravelapi.kaajbook.com/abc_laravel/public'
+
 };
