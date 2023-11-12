6184bf13c8fcd76930c093e8dabbd3da88d16a47
Arafat Hossain <arafat6462@gmail.com>
2023-11-12 17:13:33 +0600

    add dummy data to template list

--- kaajbook_angular/src/app/modules/pm/projects/pages/project-template-list/project-template-list.component.ts
+++ kaajbook_angular/src/app/modules/pm/projects/pages/project-template-list/project-template-list.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-project-template-list',
-  templateUrl: './project-template-list.component.html',
-  styleUrls: ['./project-template-list.component.scss']
+    selector: "app-project-template-list",
+    templateUrl: "./project-template-list.component.html",
+    styleUrls: ["./project-template-list.component.scss"],
 })
 export class ProjectTemplateListComponent implements OnInit {
+    // customFields = [];
+    customFields = [
+        {
+            field_label: "Create Template",
+            field_type: "text",
+        },
+        {
+            field_label: "Create Template",
+            field_type: "text",
+        },
+        {
+            field_label: "Create Template",
+            field_type: "text",
+        },
+        {
+            field_label: "Create Template",
+            field_type: "text",
+        },
+        {
+            field_label: "Create Template",
+            field_type: "text",
+        },
+        {
+            field_label: "Create Template",
+            field_type: "text",
+        },
+        {
+            field_label: "Create Template",
+            field_type: "text",
+        },
+        {
+          field_label: "Create Template",
+          field_type: "text",
+      },
+      {
+          field_label: "Create Template",
+          field_type: "text",
+      },
+      {
+          field_label: "Create Template",
+          field_type: "text",
+      },
+      {
+          field_label: "Create Template",
+          field_type: "text",
+      },
+      {
+          field_label: "Create Template",
+          field_type: "text",
+      },
+      {
+          field_label: "Create Template",
+          field_type: "text",
+      },
+      {
+          field_label: "Create Template",
+          field_type: "text",
+      },
+    ];
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
