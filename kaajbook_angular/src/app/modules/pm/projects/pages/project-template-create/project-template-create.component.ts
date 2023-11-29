7b1cf0cdf7e945a41e72eac6dca4e8d7c3d32a80
Arafat Hossain <arafat6462@gmail.com>
2023-11-29 14:31:31 +0600

    get form data

--- kaajbook_angular/src/app/modules/pm/projects/pages/project-template-create/project-template-create.component.ts
+++ kaajbook_angular/src/app/modules/pm/projects/pages/project-template-create/project-template-create.component.ts
@@ -18,8 +18,7 @@ export class ProjectTemplateCreateComponent implements OnInit {
 	public onClose: Subject<boolean>;
 	formTables: any;
 	selectboxOptions: any;
-	createCustomFieldForm: FormGroup;
-	isFormSubmitted = false;
+	createCustomTemplateForm: FormGroup;
 	isPageLoaded = false;
 
   constructor(
@@ -35,91 +34,35 @@ export class ProjectTemplateCreateComponent implements OnInit {
     this.onClose = new Subject();
 
 		this.loadForms();
-		this.getAllFormFields();
   }
 
   loadForms() {
-		this.createCustomFieldForm = this.formBuilder.group({
-			form_id: [null, Validators.required],
-			field_label: [null, Validators.required],
-			help_text: [null],
-			is_required: [false],
-			// show_on_details: [true],
-			field_type: [null, Validators.required],
-			default_value: [null],
-			select_options: this.formBuilder.array([]),
-			status: [true, Validators.required],
+		this.createCustomTemplateForm = this.formBuilder.group({
+			templateName: ['', Validators.required],
+   	 		taskInTemplate: ['', Validators.required]
 		});
 	}
 
 
-  get customFieldControl() { return this.createCustomFieldForm.controls; }
-
-	addOption() {
-		let control = <FormArray>this.createCustomFieldForm.controls.select_options;
-		control.push(
-			this.formBuilder.group({
-				value: [null, Validators.required],
-				label: [null, Validators.required],
-			})
-		)
-	}
-
-
-  deleteOption(index) {
-		let control = <FormArray>this.createCustomFieldForm.controls.select_options;
-		control.removeAt(index)
-	}
-
-	onChange(event) {
-		if (event.value == 'dropdown') {
-			this.addOption();
-			return;
-		}
-
-		const arr = <FormArray>this.createCustomFieldForm.controls.select_options;
-		arr.controls = [];
-		arr.removeAt(0);
-		arr.reset();
-	}
-
-
-  getAllFormFields() {
-		this.customFieldsService.getFormTables()
-			.subscribe(
-				data => {
-					this.formTables = data;
-				});
-	}
-
 	onSubmit() {
-		this.isFormSubmitted = true;
+		console.log(this.createCustomTemplateForm.value);
+		
+		  // Mark all controls as touched
+		  this.createCustomTemplateForm.markAllAsTouched();
 
-		if (this.createCustomFieldForm.invalid) {
+		if (this.createCustomTemplateForm.invalid) {
+			console.log('Form is invalid');
 			return;
-		}
+		  }
 
 
-    // --
-		// this.toastr.error(this.translate.instant('common.not_allowed'));
-		// this.onCancel();
-		// return;
-
-		if (this.createCustomFieldForm.value.field_type == 'dropdown') {
-			this.createCustomFieldForm.patchValue({ default_value: this.createCustomFieldForm.value.select_options });
-		}
-
-		if (this.createCustomFieldForm.value.field_type == 'checkbox') {
-			this.createCustomFieldForm.patchValue({ is_required: false });
-		}
-
-		this.customFieldsService.create(this.createCustomFieldForm.value)
-			.subscribe(
-				data => {					
-					this.toastr.success(this.translate.instant('settings.custom_fields.messages.create'), this.translate.instant('settings.custom_fields.title'));
-					this.event.emit({ data: true });
-					this.onCancel();
-				});
+		// this.customFieldsService.create(this.createCustomTemplateForm.value)
+		// 	.subscribe(
+		// 		data => {					
+		// 			this.toastr.success(this.translate.instant('settings.custom_fields.messages.create'), this.translate.instant('settings.custom_fields.title'));
+		// 			this.event.emit({ data: true });
+		// 			this.onCancel();
+		// 		});
 	}
 
 	onCancel() {
