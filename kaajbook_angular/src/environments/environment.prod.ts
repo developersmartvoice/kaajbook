d891c9f3942ee00ef29e9adcedc3a6ed072d71bf
Arafat Hossain <arafat6462@gmail.com>
2023-12-26 12:30:09 +0600

    Release 1.1.8

--- kaajbook_angular/src/environments/environment.prod.ts
+++ kaajbook_angular/src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
     // apiUrl: 'http://127.0.0.1:8000'
 
     // use this apiUrl for live server "kaajbook.com"
-    // apiUrl: 'http://kaajbook.com/kaajbook_laravel/public'
+    apiUrl: 'http://kaajbook.com/kaajbook_laravel/public'
 
     // use this apiUrl for live sub domain "test.kaajbook.com"
-    apiUrl: 'http://test.kaajbook.com/kaajbook_laravel/public'
+    // apiUrl: 'http://test.kaajbook.com/kaajbook_laravel/public'
 };
