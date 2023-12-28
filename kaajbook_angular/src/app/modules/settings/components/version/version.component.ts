8cc716d1d9ab80a5f3b920228a9f325926ef4bfd
Arafat Hossain <arafat6462@gmail.com>
2023-12-28 09:33:19 +0600

    add dummy data and design

--- kaajbook_angular/src/app/modules/settings/components/version/version.component.ts
+++ kaajbook_angular/src/app/modules/settings/components/version/version.component.ts
@@ -1,22 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { DatePipe } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { ToastrService } from 'ngx-toastr';
-import { NgxRolesService } from 'ngx-permissions';
-import { TranslateService } from '@ngx-translate/core';
 import {
-	CdkDragDrop,
-	moveItemInArray,
-	transferArrayItem
-} from '@angular/cdk/drag-drop';
-import { Subject } from 'rxjs';
-import Swal from 'sweetalert2';
-import { TodoService } from 'src/app/core/services/todo.service';
-import { AuthenticationService } from 'src/app/core/services/authentication.service';
-import { CreateTodoModalComponent } from 'src/app/modules/admin/todo/components/create-todo-modal/create-todo-modal.component';
-import { EditTodoModalComponent } from 'src/app/modules/admin/todo/components/edit-todo-modal/edit-todo-modal.component';
-
+	Component,
+	OnInit,
+	Input,
+	ViewChild
+} from '@angular/core';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 
 @Component({
   selector: 'app-version',
@@ -24,7 +12,142 @@ import { EditTodoModalComponent } from 'src/app/modules/admin/todo/components/ed
   styleUrls: ['./version.component.scss']
 })
 export class VersionComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+  public scrollConfig: PerfectScrollbarConfigInterface = {};
+	// @Input() task;
+	// @Input() loginUser: any;
+	// @Input() apiUrl;
+  task = {
+    "activities": [
+        {
+            "id": 375,
+            "user_id": 1,
+            "module": "Task",
+            "self_parent_id": 73,
+            "module_field_id": 73,
+            "action": "PUT",
+            "description": "Task Updated : <b>Container sealing</b>",
+            "ip_address": "127.0.0.1",
+            "diff_for_humans": "2 weeks before",
+            "created_at": "2023-12-10 16:22:02",
+            "updated_at": "2023-12-10 16:22:02",
+            "deleted_at": null,
+            "user": {
+                "id": 1,
+                "firstname": "FirstName",
+                "lastname": "LastName",
+                "avatar": null,
+                "full_name": "FirstName LastName"
+            }
+        },
+        {
+            "id": 226,
+            "user_id": 1,
+            "module": "Task",
+            "self_parent_id": 73,
+            "module_field_id": 73,
+            "action": "PUT",
+            "description": "Task Updated : <b>Container sealing</b>",
+            "ip_address": "127.0.0.1",
+            "diff_for_humans": "1 month before",
+            "created_at": "2023-10-29 10:54:23",
+            "updated_at": "2023-10-29 10:54:23",
+            "deleted_at": null,
+            "user": {
+                "id": 1,
+                "firstname": "FirstName",
+                "lastname": "LastName",
+                "avatar": null,
+                "full_name": "FirstName LastName"
+            }
+        },
+        {
+            "id": 225,
+            "user_id": 1,
+            "module": "Task",
+            "self_parent_id": 73,
+            "module_field_id": 73,
+            "action": "PUT",
+            "description": "Task Updated : <b>Container sealing</b>",
+            "ip_address": "127.0.0.1",
+            "diff_for_humans": "1 month before",
+            "created_at": "2023-10-29 10:53:41",
+            "updated_at": "2023-10-29 10:53:41",
+            "deleted_at": null,
+            "user": {
+                "id": 1,
+                "firstname": "FirstName",
+                "lastname": "LastName",
+                "avatar": null,
+                "full_name": "FirstName LastName"
+            }
+        },
+        {
+            "id": 224,
+            "user_id": 1,
+            "module": "Task",
+            "self_parent_id": 73,
+            "module_field_id": 73,
+            "action": "POST",
+            "description": "Task Status Changed : <b>Container sealing</b>",
+            "ip_address": "127.0.0.1",
+            "diff_for_humans": "1 month before",
+            "created_at": "2023-10-29 10:53:10",
+            "updated_at": "2023-10-29 10:53:10",
+            "deleted_at": null,
+            "user": {
+                "id": 1,
+                "firstname": "FirstName",
+                "lastname": "LastName",
+                "avatar": null,
+                "full_name": "FirstName LastName"
+            }
+        },
+        {
+            "id": 223,
+            "user_id": 1,
+            "module": "Task",
+            "self_parent_id": 73,
+            "module_field_id": 73,
+            "action": "PUT",
+            "description": "Task Updated : <b>Container sealing</b>",
+            "ip_address": "127.0.0.1",
+            "diff_for_humans": "1 month before",
+            "created_at": "2023-10-29 10:52:13",
+            "updated_at": "2023-10-29 10:52:13",
+            "deleted_at": null,
+            "user": {
+                "id": 1,
+                "firstname": "FirstName",
+                "lastname": "LastName",
+                "avatar": null,
+                "full_name": "FirstName LastName"
+            }
+        },
+        {
+            "id": 221,
+            "user_id": 1,
+            "module": "Task",
+            "self_parent_id": 73,
+            "module_field_id": 73,
+            "action": "POST",
+            "description": "Task Added : <b>Container sealing</b>",
+            "ip_address": "127.0.0.1",
+            "diff_for_humans": "1 month before",
+            "created_at": "2023-10-29 10:51:08",
+            "updated_at": "2023-10-29 10:51:08",
+            "deleted_at": null,
+            "user": {
+                "id": 1,
+                "firstname": "FirstName",
+                "lastname": "LastName",
+                "avatar": null,
+                "full_name": "FirstName LastName"
+            }
+        }
+    ],
+}
+
+	constructor() {}
+
+	ngOnInit() {}
 }
