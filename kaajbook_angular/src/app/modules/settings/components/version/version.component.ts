069c90949e51074a9a631b0b15e97642df902d21
Arafat Hossain <arafat6462@gmail.com>
2024-02-14 11:57:33 +0600

    add download option for download all version log as pdf

--- kaajbook_angular/src/app/modules/settings/components/version/version.component.ts
+++ kaajbook_angular/src/app/modules/settings/components/version/version.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input, ViewChild } from "@angular/core";
 import { PerfectScrollbarConfigInterface } from "ngx-perfect-scrollbar";
 import versionLog from 'src/assets/version';
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import html2pdf from 'html2pdf.js';
 
 @Component({
     selector: "app-version",
@@ -16,4 +19,24 @@ export class VersionComponent implements OnInit {
     constructor() {}
 
     ngOnInit() {}
+    downloadAllVersionLogAsPDF() {
+        const content = document.getElementById('versionLogs').innerHTML;
+        const downloadDateTime = new Date().toLocaleString(); // Get the current date and time
+
+        const contentWithDateTime = `
+            <div>
+                <p>Date and Time: ${downloadDateTime}</p>
+                ${content}
+            </div>
+        `;
+
+        const options = {
+            filename: 'version_logs.pdf',
+            image: { type: 'jpeg', quality: 0.9 },
+            html2canvas: { scale: 3 },
+            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+        };
+        html2pdf().set(options).from(contentWithDateTime).save();
+    }
+
 }
